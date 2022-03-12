b998e52600af6071023ae71c5f6f8a38e1eda659
Dawid Rodak <dawidrodak@yahoo.com>
2022-03-12 13:45:24 +0100

    Add fonts

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,16 @@
+import { H2 } from "~/components/typography";
+
 export default function Index() {
     return (
         <div>
-            <h1 className="text-violet-400">Hello world</h1>
+            <div className="p-10">
+                <H2>blog pupu</H2>
+                <ul className="mt-5 text-lg">
+                    <li>post 1</li>
+                    <li>post 2</li>
+                    <li>post 3</li>
+                </ul>
+            </div>
         </div>
     );
 }
