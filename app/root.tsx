4f1abde1cd1ee4559bf6c39439de88fe3ced526e
Dawid Rodak <dawidrodak@yahoo.com>
2022-03-12 11:58:17 +0100

    Boilerplate

--- app/root.tsx
+++ app/root.tsx
@@ -1,32 +1,48 @@
 import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
+    Links,
+    LiveReload,
+    Meta,
+    Outlet,
+    Scripts,
+    ScrollRestoration,
 } from "remix";
+
 import type { MetaFunction } from "remix";
+import { LinksFunction } from "@remix-run/react/routeModules";
+
+import styles from "./styles/app.css";
 
 export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
+    return { title: "New Remix App" };
+};
+
+export const links: LinksFunction = () => {
+    return [
+        {
+            rel: "stylesheet",
+            href: styles,
+        },
+    ];
 };
 
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <head>
+                <meta charSet="utf-8" />
+                <meta
+                    name="viewport"
+                    content="width=device-width,initial-scale=1"
+                />
+                <Meta />
+                <Links />
+            </head>
+            <body>
+                <Outlet />
+                <ScrollRestoration />
+                <Scripts />
+                <LiveReload />
+            </body>
+        </html>
+    );
 }
